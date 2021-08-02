09bf6ec787773e3b8815fbdae74cdccc29cc317c
paritakumbhani <46765796+paritakumbhani@users.noreply.github.com>
2021-08-02 13:04:20 -0400

    Create MainClass.java

---
+++ Assignment/src/Assignment3_3/MainClass.java
@@ -0,0 +1,29 @@
+package Assignment3_3;
+
+
+abstract class X {
+
+	public X() {
+		System.out.println("ONE");
+	}
+	
+	abstract void abstractMethod();
+}
+
+class Y extends X {
+	public Y() {
+		System.out.println("TWO");
+	}
+	
+	@Override
+	void abstractMethod() {
+		System.out.println("THREE");
+	}
+}
+
+public class MainClass{
+	public static void main (String[] args) {
+		X x = new Y();
+		x.abstractMethod();
+	}
+}
